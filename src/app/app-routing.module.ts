ec061ca2b00e40c6d74fd374da7c51edf88026f3
Moustafa Mohsen <moustafamohsen1@gmail.com>
2018-06-24 04:56:46 +0200

    generated service and updated html&style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UsersComponent} from './users/users.component';
+import {DetailsComponent} from './details/details.component';
+import {PostsComponent} from './posts/posts.component';
 
-const routes: Routes = [];
+
+ const routes: Routes = [
+   {path:'',component:UsersComponent},
+   {path:'details:id',component:DetailsComponent},
+   {path:'posts',component:PostsComponent}
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}
